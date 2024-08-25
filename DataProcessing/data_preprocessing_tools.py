0f051b578edddedce4b8b69b3c338be95d476f0e
Nhan Le <nhanle0829.work@gmail.com>
2024-08-24 19:21:26 -0700

    import dataset

--- DataProcessing/data_preprocessing_tools.py
+++ DataProcessing/data_preprocessing_tools.py
@@ -1,3 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+dataset = pd.read_csv("Data.csv")
+X = dataset.iloc[:, :-1].values   # Feature variables data
+Y = dataset.iloc[:, -1].values   # Dependent variables data
+print(X)
+print(Y)
