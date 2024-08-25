b51aa34573e1793f94a450cac10fc86eff42b368
Nhan Le <nhanle0829.work@gmail.com>
2024-08-25 00:21:17 -0700

    encode independent variable and improve printing format

--- DataProcessing/data_preprocessing_tools.py
+++ DataProcessing/data_preprocessing_tools.py
@@ -5,11 +5,19 @@ import pandas as pd
 dataset = pd.read_csv("Data.csv")
 X = dataset.iloc[:, :-1].values   # Feature variables data
 y = dataset.iloc[:, -1].values   # Dependent variables data
-print(X)
-print(y)
+print("Importing dataset")
+print(f"{X}\n\n{y}\n")
 
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-imputer = imputer.fit(X[:, 1:3])
+imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
-print(X)
+print("Taking care of missing values")
+print(f"{X}\n")
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[("encoder", OneHotEncoder(), [0])], remainder="passthrough")
+X = np.array(ct.fit_transform(X))
+print("Encoding independent variable")
+print(f"{X}\n")
