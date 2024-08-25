73d6972dc86b174d21697ac0098db3fb301b1956
Nhan Le <nhanle0829.work@gmail.com>
2024-08-24 20:27:48 -0700

    take care of missing values

--- DataProcessing/data_preprocessing_tools.py
+++ DataProcessing/data_preprocessing_tools.py
@@ -4,6 +4,12 @@ import pandas as pd
 
 dataset = pd.read_csv("Data.csv")
 X = dataset.iloc[:, :-1].values   # Feature variables data
-Y = dataset.iloc[:, -1].values   # Dependent variables data
+y = dataset.iloc[:, -1].values   # Dependent variables data
 print(X)
-print(Y)
+print(y)
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+print(X)
