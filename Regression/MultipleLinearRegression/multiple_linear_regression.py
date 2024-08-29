8d1c8bd3ad9f3ac197da41b7544144b0dbd00a39
Nhan Le <nhanle0829.work@gmail.com>
2024-08-29 01:02:53 -0700

    encode categorical feature variable

--- Regression/MultipleLinearRegression/multiple_linear_regression.py
+++ Regression/MultipleLinearRegression/multiple_linear_regression.py
@@ -6,3 +6,8 @@ dataset = pd.read_csv("50_Startups.csv")
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[("encoder", OneHotEncoder(), [-1])], remainder="passthrough")
+X = np.array(ct.fit_transform(X))
+
