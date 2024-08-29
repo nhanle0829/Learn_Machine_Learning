0109089bc3cd33f8b05a9f256a74ea425ab839e4
Nhan Le <nhanle0829.work@gmail.com>
2024-08-29 01:06:02 -0700

    split dataset into train and test set

--- Regression/MultipleLinearRegression/multiple_linear_regression.py
+++ Regression/MultipleLinearRegression/multiple_linear_regression.py
@@ -11,3 +11,5 @@ from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[("encoder", OneHotEncoder(), [-1])], remainder="passthrough")
 X = np.array(ct.fit_transform(X))
 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
