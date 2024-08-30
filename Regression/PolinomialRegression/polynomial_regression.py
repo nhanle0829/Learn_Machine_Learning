c65eddeb1202baeb18a45b4a38e872ee0258ecd3
Nhan Le <nhanle0829.work@gmail.com>
2024-08-29 19:17:07 -0700

    train polynomial regression model

--- Regression/PolinomialRegression/polynomial_regression.py
+++ Regression/PolinomialRegression/polynomial_regression.py
@@ -9,3 +9,9 @@ y = dataset.iloc[:, -1].values
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
+
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree=2)
+X_poly = poly_reg.fit_transform(X)
+lin_reg2 = LinearRegression()
+lin_reg2.fit(X_poly, y)
