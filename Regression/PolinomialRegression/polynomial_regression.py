33cd99f00e1886cb6bd8f0103a9753d87413472a
Nhan Le <nhanle0829.work@gmail.com>
2024-08-29 18:50:15 -0700

    train linear regression model

--- Regression/PolinomialRegression/polynomial_regression.py
+++ Regression/PolinomialRegression/polynomial_regression.py
@@ -6,3 +6,6 @@ dataset = pd.read_csv("Position_Salaries.csv")
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
