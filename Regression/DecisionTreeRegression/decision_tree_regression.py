f8ac911d93bf2aae9e91801469e84bdb024e1656
Nhan Le <nhanle0829.work@gmail.com>
2024-08-31 03:23:27 -0700

    visual Decision Tree Regression result

--- Regression/DecisionTreeRegression/decision_tree_regression.py
+++ Regression/DecisionTreeRegression/decision_tree_regression.py
@@ -11,3 +11,12 @@ regressor = DecisionTreeRegressor(random_state=0)
 regressor.fit(X, y)
 
 print(regressor.predict([[6.5]]))
+
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape(-1, 1)
+plt.scatter(X, y, color="red")
+plt.plot(X_grid, regressor.predict(X_grid), color="blue")
+plt.title("Decision Tree Regression Model")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
