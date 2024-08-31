4815957ceb9957b65bbcd079fa202bbc885aac94
Nhan Le <nhanle0829.work@gmail.com>
2024-08-31 03:11:51 -0700

    train Decision Tree Regression model

--- Regression/DecisionTreeRegression/decision_tree_regression.py
+++ Regression/DecisionTreeRegression/decision_tree_regression.py
@@ -6,3 +6,7 @@ dataset = pd.read_csv("Position_Salaries.csv")
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X, y)
+
