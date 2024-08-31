7a07fa300fe8e9aab4d877b1bea19607905cca8f
Nhan Le <nhanle0829.work@gmail.com>
2024-08-31 00:10:17 -0700

    import dataset

---
+++ Regression/SupportVectorRegression/support_vector_regression.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("Position_Salaries.csv")
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+print
