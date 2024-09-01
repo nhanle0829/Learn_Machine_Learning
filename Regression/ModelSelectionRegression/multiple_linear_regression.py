71ef7cc3f79a889b0d6f2151480927360361e14f
Nhan Le <nhanle0829.work@gmail.com>
2024-09-01 14:15:37 -0700

    complete general template for multiple linear regression model

---
+++ Regression/ModelSelectionRegression/multiple_linear_regression.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("Data.csv") #Replace "Data.csv" with your dataset name
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_test.reshape(-1, 1), y_pred.reshape(-1, 1)), axis=1))
+
+from sklearn.metrics import r2_score
+print(r2_score(y_test, y_pred))
