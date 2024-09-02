09efaec7b228ae36852ce840adedc31902c174b0
Nhan Le <nhanle0829.work@gmail.com>
2024-09-02 02:14:58 -0700

    scale independent variable

--- Classification/Logistic_Regression/logistic_regression.py
+++ Classification/Logistic_Regression/logistic_regression.py
@@ -9,3 +9,7 @@ y = dataset.iloc[:, -1].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
