dba03d7c7c5e91081e5c1c85a89d97cd4772c9f5
Nhan Le <nhanle0829.work@gmail.com>
2024-09-01 23:20:04 -0700

    import dataset and split into train and test set

---
+++ Classification/Logistic_Regression/logistic_regression.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("Social_Network_Ads.csv")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
