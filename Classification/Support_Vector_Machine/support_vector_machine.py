4023c46c265fb4f29f07293dc07414945028d958
Nhan Le <nhanle0829.work@gmail.com>
2024-09-03 02:55:07 -0700

    predict with Support Vector Machine model

--- Classification/Support_Vector_Machine/support_vector_machine.py
+++ Classification/Support_Vector_Machine/support_vector_machine.py
@@ -17,3 +17,8 @@ X_test = sc.transform(X_test)
 from sklearn.svm import SVC
 classifier = SVC(kernel="linear", random_state=0)
 classifier.fit(X_train, y_train)
+
+print(classifier.predict(sc.transform([[30, 87000]])))
+
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_test.reshape(-1, 1), y_pred.reshape(-1, 1)), axis=1))
