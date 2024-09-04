ada02c0e23fd81a18ba4b27a08be68e417279586
Nhan Le <nhanle0829.work@gmail.com>
2024-09-03 21:21:31 -0700

    create confusion matrix and calculate accuracy score for Kernel SVM model

--- Classification/Kernel_SVM/kernel_svm.py
+++ Classification/Kernel_SVM/kernel_svm.py
@@ -22,3 +22,7 @@ print(classifier.predict(sc.transform([[30, 87000]])))
 
 y_pred = classifier.predict(X_test)
 print(np.concatenate((y_test.reshape(-1, 1), y_pred.reshape(-1, 1)), axis=1))
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+print(confusion_matrix(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
