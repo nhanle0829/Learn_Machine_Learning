465ab61a0da6860b690ba125a80efb0a5ac3fbd2
Nhan Le <nhanle0829.work@gmail.com>
2024-09-06 03:37:20 -0700

    predict with Decision Tree Classification model

--- Classification/DecisionTreeClassification/decision_tree_classification.py
+++ Classification/DecisionTreeClassification/decision_tree_classification.py
@@ -7,10 +7,17 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion="entropy", random_state=0)
+classifier.fit(X_train, y_train)
+
+print(classifier.predict(sc.transform([[30,87000]])))
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(-1,1), y_test.reshape(-1,1)), axis=1))
