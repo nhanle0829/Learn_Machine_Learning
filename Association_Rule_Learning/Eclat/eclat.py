75f43967d60a568bf49818100df5d3605d9d5d15
Nhan Le <nhanle0829.work@gmail.com>
2024-09-17 12:58:21 -0700

    import and preprocess market dataset

---
+++ Association_Rule_Learning/Eclat/eclat.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv("Market_Basket_Optimisation.csv", header=None)
+transactions = []
+for i in range(7501):
+    transactions.append([str(dataset.values[i, j]) for j in range(20)])
