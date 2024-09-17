e7cb75c0c814dad5046a59ba0ce399af76fc751b
Nhan Le <nhanle0829.work@gmail.com>
2024-09-17 13:19:39 -0700

    visualize result for Eclat model

--- Association_Rule_Learning/Eclat/eclat.py
+++ Association_Rule_Learning/Eclat/eclat.py
@@ -9,3 +9,14 @@ for i in range(7501):
 
 from apyori import apriori
 rules = apriori(transactions=transactions, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2, max_length=2)
+
+results = list(rules)
+print(results)
+
+def inspect(results):
+    lhs         = [tuple(result[2][0][0])[0] for result in results]
+    rhs         = [tuple(result[2][0][1])[0] for result in results]
+    supports    = [result[1] for result in results]
+    return list(zip(lhs, rhs, supports))
+results_in_DataFrame = pd.DataFrame(inspect(results), columns = ['Product 1', 'Product 2', 'Support'])
+print(results_in_DataFrame.nlargest(n=10, columns="Support"))
