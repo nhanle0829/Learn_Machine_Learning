73ffe43d604eb25e99a1e5b2078b356ea3089c61
Nhan Le <nhanle0829.work@gmail.com>
2024-09-17 01:08:45 -0700

    train Apriori model

--- Association_Rule_Learning/Apriori/apriori.py
+++ Association_Rule_Learning/Apriori/apriori.py
@@ -6,3 +6,6 @@ dataset = pd.read_csv("Market_Basket_Optimisation.csv", header=None)
 transactions = []
 for i in range(7501):
     transactions.append([str(dataset.values[i, j])for j in range(20)])
+
+from apyori import apriori
+rules = apriori(transactions=transactions, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2, max_length=2)
