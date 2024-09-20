59b0365f096bd5ce1c9f21faa1a47979b111cef4
Nhan Le <nhanle0829.work@gmail.com>
2024-09-20 03:33:43 -0700

    predict with K Nearest Neighbors model

--- Natural_Language_Processing/natural_language_processing.py
+++ Natural_Language_Processing/natural_language_processing.py
@@ -34,8 +34,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # classifier.fit(X_train, y_train)
 
 # Decision Tree Classification Model
-from sklearn.tree import DecisionTreeClassifier
-classifier = DecisionTreeClassifier(criterion="entropy", random_state=0)
+# from sklearn.tree import DecisionTreeClassifier
+# classifier = DecisionTreeClassifier(criterion="entropy", random_state=0)
+# classifier.fit(X_train, y_train)
+
+# K Nearest Neighbors Model
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5, p=2, metric="minkowski")
 classifier.fit(X_train, y_train)
 
 y_pred = classifier.predict(X_test)
