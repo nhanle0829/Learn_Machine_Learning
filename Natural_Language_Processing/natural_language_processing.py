8c257e58706bff0f63852fc5a21f87ae71af1256
Nhan Le <nhanle0829.work@gmail.com>
2024-09-20 03:23:04 -0700

    train and predict with Naive Bayes model

--- Natural_Language_Processing/natural_language_processing.py
+++ Natural_Language_Processing/natural_language_processing.py
@@ -24,3 +24,13 @@ from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features=1500)
 X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(-1, 1), y_test.reshape(-1, 1)), axis=1))
