6f868079ba95563844d6123b0539252109a6504e
Nhan Le <nhanle0829.work@gmail.com>
2024-09-20 03:02:53 -0700

    clean the text

--- Natural_Language_Processing/natural_language_processing.py
+++ Natural_Language_Processing/natural_language_processing.py
@@ -3,3 +3,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 dataset = pd.read_csv("Restaurant_Reviews.tsv", delimiter='\t', quoting=3)
+
+import re
+import nltk
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(1000):
+    review = re.sub("[^a-zA-Z]", " ", dataset["Review"][i])
+    review = review.lower().split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if word not in set(stopwords.words("english"))]
+    review = ' '.join(review)
+    corpus.append(review)
