b003a1f7a19f44b9ce2f93d1c8efcbc3d1ee6ad7
Nhan Le <nhanle0829.work@gmail.com>
2024-09-20 03:31:10 -0700

    predict with Decision Tree Classification model

--- Natural_Language_Processing/natural_language_processing.py
+++ Natural_Language_Processing/natural_language_processing.py
@@ -28,8 +28,14 @@ y = dataset.iloc[:, -1].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-from sklearn.naive_bayes import GaussianNB
-classifier = GaussianNB()
+# Naive Bayes Model
+# from sklearn.naive_bayes import GaussianNB
+# classifier = GaussianNB()
+# classifier.fit(X_train, y_train)
+
+# Decision Tree Classification Model
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion="entropy", random_state=0)
 classifier.fit(X_train, y_train)
 
 y_pred = classifier.predict(X_test)
