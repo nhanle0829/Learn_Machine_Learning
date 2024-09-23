df9bef5764515c937f1609416d0d75bc01a582cf
Nhan Le <nhanle0829.work@gmail.com>
2024-09-23 03:06:03 -0700

    import and preprocess churn modelling dataset

---
+++ Deep_Learning/Artificial_Neural_Network/artificial_neural_network.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+print(tf.__version__)
+
+dataset = pd.read_csv("Churn_Modelling.csv")
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[("encoder", OneHotEncoder(), [1])], remainder="passthrough")
+X = np.array(ct.fit_transform(X))
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
