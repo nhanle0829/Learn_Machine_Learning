d3d6b4bb532266060a51e373d278d9f03559d0ae
Nhan Le <nhanle0829.work@gmail.com>
2024-09-23 03:41:19 -0700

    predict with the ann

--- Deep_Learning/Artificial_Neural_Network/artificial_neural_network.py
+++ Deep_Learning/Artificial_Neural_Network/artificial_neural_network.py
@@ -35,3 +35,10 @@ ann.add(keras.layers.Dense(units=1, activation="sigmoid"))
 
 ann.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
 ann.fit(X_train, y_train, batch_size=32, epochs=100)
+
+ann.predict(sc.transform([[1, 0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]]) > 0.5)
+
+y_pred = ann.predict(X_test)
+y_pred = y_pred > 0.5
+print(np.concatenate((y_test.reshape(-1, 1), y_pred.reshape(-1, 1)), axis=1))
+
