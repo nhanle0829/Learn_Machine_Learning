664dad1dd6afc9a4ad3aa440ad02db631b7f3f3e
Nhan Le <nhanle0829.work@gmail.com>
2024-09-25 15:19:30 -0700

    create confusion matrix and calculate accuracy score

--- Dimensionality_Reduction/Linear_Discriminant_Analysis/linear_discriminant_analysis.py
+++ Dimensionality_Reduction/Linear_Discriminant_Analysis/linear_discriminant_analysis.py
@@ -7,7 +7,7 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
@@ -16,9 +16,14 @@ X_test = scaler.transform(X_test)
 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 lda = LinearDiscriminantAnalysis(n_components=2)
-X_train = lda.fit_transform(X_train)
+X_train = lda.fit_transform(X_train, y_train)
 X_test = lda.transform(X_test)
 
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state=0)
 classifier.fit(X_train, y_train)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred = classifier.predict(X_test)
+print(confusion_matrix(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
