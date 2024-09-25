4716c53e9a4145c6a1f064505c8a66a9ff9f8c4c
Nhan Le <nhanle0829.work@gmail.com>
2024-09-25 15:32:40 -0700

    create confusion matrix and calculate accuracy score

--- Dimensionality_Reduction/Kernel_PCA/kernel_pca.py
+++ Dimensionality_Reduction/Kernel_PCA/kernel_pca.py
@@ -23,3 +23,7 @@ from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state=0)
 classifier.fit(X_train, y_train)
 
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred = classifier.predict(X_test)
+print(confusion_matrix(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
