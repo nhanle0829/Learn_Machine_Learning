c7e9169238178f414ec522dc8b3864f0a4b478e1
Nhan Le <nhanle0829.work@gmail.com>
2024-09-25 02:31:55 -0700

    train the CNN

--- Deep_Learning/Convolutional_Neural_Network/convolutional_neural_network.py
+++ Deep_Learning/Convolutional_Neural_Network/convolutional_neural_network.py
@@ -54,3 +54,11 @@ cnn.add(keras.layers.Dense(units=128, activation="relu"))
 
 # Step 5 - Output Layer
 cnn.add(keras.layers.Dense(units=1, activation="sigmoid"))
+
+# Part 3 - Training the CNN
+
+# Compiling the CNN
+cnn.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+# Training the CNN on the Training set and evaluating it on the Test set
+cnn.fit(x=training_set, validation_data=test_set, epochs=25)
