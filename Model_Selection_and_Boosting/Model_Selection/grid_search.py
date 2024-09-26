f2d630f4f07da34d77191c0f067721d496951e68
Nhan Le <nhanle0829.work@gmail.com>
2024-09-25 18:48:31 -0700

    applyy grid search to find the best model

--- Model_Selection_and_Boosting/Model_Selection/grid_search.py
+++ Model_Selection_and_Boosting/Model_Selection/grid_search.py
@@ -37,7 +37,15 @@ print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
 print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
 
 # Applying Grid Search to find the best model and the best parameters
-
+from sklearn.model_selection import GridSearchCV
+parameters = [{"C": [0.25, 0.5, 0.75, 1], "kernel": ["linear"]},
+              {"C": [0.25, 0.5, 0.75, 1], "kernel": ["rbf"], "gamma": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]}]
+grid_search = GridSearchCV(estimator=classifier, param_grid=parameters, scoring="accuracy", cv=10, n_jobs=-1)
+grid_search.fit(X_train, y_train)
+best_accuracy = grid_search.best_score_
+best_parameters = grid_search.best_params_
+print("Best Accuracy: {:.2f} %".format(best_accuracy*100))
+print("Best Parameters:", best_parameters)
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
