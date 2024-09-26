d902dc58bbda37100065c6b16b9233620bbd6899
Nhan Le <nhanle0829.work@gmail.com>
2024-09-25 20:37:53 -0700

    create confusion matrix and calculate accuracy score

--- Model_Selection_and_Boosting/XGBoost/xg_boost.py
+++ Model_Selection_and_Boosting/XGBoost/xg_boost.py
@@ -12,4 +12,9 @@ from xgboost import XGBClassifier
 classifier = XGBClassifier()
 classifier.fit(X_train, X_test)
 
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred = classifier.predict(X_test)
+print(confusion_matrix(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
+
 
