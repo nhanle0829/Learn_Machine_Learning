b9ee39c6a374586ae4c7ba33ad37ba175d515a73
Nhan Le <nhanle0829.work@gmail.com>
2024-09-25 20:46:30 -0700

    apply k-Fold Cross Validation

--- Model_Selection_and_Boosting/XGBoost/xg_boost.py
+++ Model_Selection_and_Boosting/XGBoost/xg_boost.py
@@ -8,13 +8,21 @@ y = dataset.iloc[:, -1].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y_train = le.fit_transform(y_train)
+y_test = le.transform(y_test)
+
 from xgboost import XGBClassifier
 classifier = XGBClassifier()
-classifier.fit(X_train, X_test)
+classifier.fit(X_train, y_train)
 
 from sklearn.metrics import confusion_matrix, accuracy_score
 y_pred = classifier.predict(X_test)
 print(confusion_matrix(y_test, y_pred))
 print(accuracy_score(y_test, y_pred))
 
-
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10)
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
